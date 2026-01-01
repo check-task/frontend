866fc43668c915a00b9efdef361cb53ea20d499b
성태경 <taegyeong0320@naver.com>
2026-01-02 02:27:13 +0900

    chore: Zustand 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+'use client';
+
 import { css } from '../../styled-system/css';
 import { Button } from '@/components/ui';
+import { useCounterStore } from '@/providers/counter-store-provider';
 
 export default function Home() {
+  const { count, incrementCount, decrementCount } = useCounterStore(
+    (state) => state,
+  );
+
   return (
     <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
       <Button>Button</Button>
       Hello 🐼!
+      <div>
+        Count: {count}
+        <hr />
+        <button type='button' onClick={incrementCount}>
+          Increment Count
+        </button>
+        <button type='button' onClick={decrementCount}>
+          Decrement Count
+        </button>
+      </div>
     </div>
   );
 }
