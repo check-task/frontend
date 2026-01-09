e175b524072066d9cdf1131f82fbbf1e34397dc4
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-01-09 14:55:07 +0900

    feat: 모달 체크버튼 아이콘 추가

---
+++ src/components/icons/ModalCheckIcon.tsx
@@ -0,0 +1,23 @@
+export const ModalCheckIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='28'
+    height='28'
+    viewBox='0 0 28 28'
+    fill='none'
+  >
+    <circle
+      cx='14'
+      cy='14'
+      r='9.91667'
+      stroke='#090A0B'
+      strokeWidth='1.16667'
+    />
+    <path
+      d='M9.33301 14.0007L11.9588 17.9393C12.1897 18.2856 12.6986 18.2856 12.9295 17.9393L18.6663 9.33398'
+      stroke='#090A0B'
+      strokeWidth='1.16667'
+      strokeLinecap='round'
+    />
+  </svg>
+);
