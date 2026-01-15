d71c54680741a9948aceebf175303ef17cb202cf
성태경 <taegyeong0320@naver.com>
2026-01-16 02:10:26 +0900

    feat: AddIcon 컴포넌트 추가 및 적용

---
+++ src/components/icons/AddIcon.tsx
@@ -0,0 +1,14 @@
+export const AddIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+    >
+      <path d='M4 12H20' stroke='black' strokeLinecap='round' />
+      <path d='M12 4V20' stroke='black' strokeLinecap='round' />
+    </svg>
+  );
+};
--- src/features/profile/components/AddFolderButton.tsx
+++ src/features/profile/components/AddFolderButton.tsx
@@ -2,7 +2,7 @@
 
 import { useModalStore } from '@/stores/modal-store';
 import { FolderModalContent } from './FolderModalContent';
-import { PlusIcon } from '@/components/icons/PlusIcon';
+import { AddIcon } from '@/components/icons/AddIcon';
 
 export const AddFolderButton = () => {
   const openModal = useModalStore((state) => state.openModal);
@@ -17,7 +17,7 @@ export const AddFolderButton = () => {
 
   return (
     <button onClick={handleOpen} style={{ cursor: 'pointer' }}>
-      <PlusIcon />
+      <AddIcon />
     </button>
   );
 };
