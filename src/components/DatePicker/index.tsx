c499ca81c30170f66e23097526cdbb27ff8b18b0
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-01-16 01:40:14 +0900

    feat: 날짜 표시 추가

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -17,6 +17,17 @@ export default function DatePicker() {
     console.log(`확정된 날짜 확인: ${date.toLocaleDateString()}`);
   };
 
+  const formatDate = (date: Date) => {
+    return date
+      .toLocaleDateString('ko-KR', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .replace(/\s/g, '')
+      .slice(0, -1);
+  };
+
   return (
     <div className={containerStyle}>
       {/* 캘린더 아이콘 버튼 처리 */}
@@ -27,6 +38,9 @@ export default function DatePicker() {
       >
         <CalenderIcon />
       </button>
+      <div>
+        <span className={dateTextStyle}>{formatDate(confirmedDate)}</span>
+      </div>
 
       {/* 달력 모달 */}
       {isOpen && (
@@ -44,5 +58,14 @@ export default function DatePicker() {
 // 버튼과 모달 전체 컨테이너
 const containerStyle = css({
   position: 'relative',
-  display: 'inline-block',
+  display: 'inline-flex',
+  alignItems: 'center',
+  gap: '0.5rem',
+});
+
+// 날짜 글자 스타일
+const dateTextStyle = css({
+  textStyle: 'body1.r',
+  color: 'gray.600',
+  cursor: 'default',
 });
