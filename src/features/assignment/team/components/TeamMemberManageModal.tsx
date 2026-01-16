ea04acbce57bea8dc0e2122a44b0640085ce4f6a
HyunwooDoo <engusdn0421@gmail.com>
2026-01-16 15:41:10 +0900

    feat: 팀원 관리 모달 구현

---
+++ src/features/assignment/team/components/TeamMemberManageModal.tsx
@@ -0,0 +1,38 @@
+import { css } from 'styled-system/css';
+import { TeamInviteButton } from './TeamInviteButton';
+import { TeamMemberManageModalItem } from './TeamMemberManageModalItem';
+
+export const TeamMemberManageModal = () => {
+  return (
+    <div className={modalContentStyle}>
+      <TeamInviteButton />
+      <TeamMemberManageModalItem
+        nickname='멤버 닉네임'
+        role='Owner'
+        onSetLeader={() => console.log('팀장으로 설정')}
+        onDeleteMember={() => console.log('팀원 삭제')}
+      />
+
+      <TeamMemberManageModalItem
+        nickname='멤버 닉네임'
+        role='Member'
+        onSetLeader={() => console.log('팀장으로 설정')}
+        onDeleteMember={() => console.log('팀원 삭제')}
+      />
+
+      <TeamMemberManageModalItem
+        nickname='멤버 닉네임'
+        role='Member'
+        onSetLeader={() => console.log('팀장으로 설정')}
+        onDeleteMember={() => console.log('팀원 삭제')}
+      />
+    </div>
+  );
+};
+
+const modalContentStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+  pt: '1.75rem',
+});
