7bb3e1c1b43f1e2d5f9e965fdf5751e18d9f23b9
HyunwooDoo <engusdn0421@gmail.com>
2026-01-16 15:41:18 +0900

    feat: 팀원 관리 모달 적용

--- src/features/assignment/team/components/TeamHeaderButtonGroup.tsx
+++ src/features/assignment/team/components/TeamHeaderButtonGroup.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { useModalStore } from '@/stores/modal-store';
 import { useUIStore } from '@/stores/ui-store';
 import { css } from 'styled-system/css';
+import { TeamMemberManageModal } from './TeamMemberManageModal';
 
 export const TeamHeaderButton = () => {
   const isSidebarCollapsed = useUIStore((state) => state.isSidebarCollapsed);
+  const openModal = useModalStore((state) => state.openModal);
+
+  const handleTeamMemberManageClick = () => {
+    openModal({
+      title: '팀원 관리',
+      content: <TeamMemberManageModal />,
+    });
+  };
 
   return (
     <div
@@ -20,6 +30,7 @@ export const TeamHeaderButton = () => {
         className={css({
           transition: 'width 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
         })}
+        onClick={handleTeamMemberManageClick}
       >
         팀원관리
       </Button>
