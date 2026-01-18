d8cb2b644ea37335bc1ccb0b5c9124a7f5faa7f3
성태경 <taegyeong0320@naver.com>
2026-01-18 20:21:50 +0900

    refactor: MonthPickerIcon 다크모드 적용

--- src/components/icons/MonthPickerIcon.tsx
+++ src/components/icons/MonthPickerIcon.tsx
@@ -1,4 +1,9 @@
+import { css } from 'styled-system/css';
+
 export const MonthPickerIcon = () => {
+  const iconStyle = css({
+    stroke: 'gray.900',
+  });
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -9,7 +14,7 @@ export const MonthPickerIcon = () => {
     >
       <path
         d='M36 20L25.4142 30.5858C24.6332 31.3668 23.3668 31.3668 22.5858 30.5858L12 20'
-        stroke='#090A0B'
+        className={iconStyle}
         strokeWidth='2'
         strokeLinecap='round'
       />
