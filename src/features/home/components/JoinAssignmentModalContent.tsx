25d76c9ce581b09b33a79ce654e084e7dd799b12
성태경 <taegyeong0320@naver.com>
2026-01-25 15:22:15 +0900

    feat: 초대코드 미입력시 버튼 비활성화

--- src/features/home/components/JoinAssignmentModalContent.tsx
+++ src/features/home/components/JoinAssignmentModalContent.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/TextField';
 import { useModalStore } from '@/stores/modal-store';
@@ -6,6 +7,7 @@ import { stack } from 'styled-system/patterns';
 
 export const JoinAssignmentModalContent = () => {
   const closeModal = useModalStore((state) => state.closeModal);
+  const [inviteCode, setInviteCode] = useState('');
 
   const handleJoin = () => {
     closeModal();
@@ -19,10 +21,17 @@ export const JoinAssignmentModalContent = () => {
           size='modal'
           type='text'
           placeholder='공유받은 코드를 붙여 넣으세요.'
+          value={inviteCode}
+          onChange={(e) => setInviteCode(e.target.value)}
         />
       </FormField>
 
-      <Button variant='fillBlue' size='large' onClick={handleJoin}>
+      <Button
+        variant='fillBlue'
+        size='large'
+        onClick={handleJoin}
+        disabled={!inviteCode.trim()}
+      >
         팀과제 참여
       </Button>
     </Container>
