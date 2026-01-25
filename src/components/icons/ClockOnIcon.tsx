6a76d2f88155b48e4041146f36d05702d2290f75
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-01-26 06:04:14 +0900

    refactor: Clock아이콘 스타일 수정

--- src/components/icons/ClockOnIcon.tsx
+++ src/components/icons/ClockOnIcon.tsx
@@ -3,6 +3,8 @@ import { css } from 'styled-system/css';
 export const ClockOnIcon = () => {
   const iconStyle = css({
     stroke: 'blue.500',
+    strokeWidth: '1.33333',
+    strokeLinecap: 'round',
   });
 
   return (
@@ -15,21 +17,9 @@ export const ClockOnIcon = () => {
       className={iconStyle}
     >
       <circle cx='15.9998' cy='17.3333' r='9.33333' strokeWidth='1.33333' />
-      <path
-        d='M9.3335 5.33398L5.3335 9.33398'
-        strokeWidth='1.33333'
-        strokeLinecap='round'
-      />
-      <path
-        d='M22.6665 5.33398L26.6665 9.33398'
-        strokeWidth='1.33333'
-        strokeLinecap='round'
-      />
-      <path
-        d='M16 10.666V17.3327L20 22.666'
-        strokeWidth='1.33333'
-        strokeLinecap='round'
-      />
+      <path d='M9.3335 5.33398L5.3335 9.33398' />
+      <path d='M22.6665 5.33398L26.6665 9.33398' />
+      <path d='M16 10.666V17.3327L20 22.666' />
     </svg>
   );
 };
