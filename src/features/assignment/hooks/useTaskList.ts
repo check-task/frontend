0d09542bd830b89d48061627a89ef53dad880a56
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-02-05 11:32:57 +0900

    feat: 과제 목록 조회 커스텀 훅 추가

---
+++ src/features/assignment/hooks/useTaskList.ts
@@ -0,0 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
+import { getTaskList } from '@/services/task';
+import { FOLDER_COLORS, FolderColor } from '@/types/folder';
+
+// 과제 목록 화면에서 사용하는 타입만 정의
+// assignmentCard에서 사용하는 타입 형태로 매핑
+export interface TaskListItem {
+  id: number;
+  type: 'personal' | 'team';
+  folderName: string;
+  assignmentName: string;
+  dueDate: string;
+  folderColor: FolderColor;
+  // dateType은 assignmentList에서 전달
+}
+
+// 과제 목록 조회 커스텀 훅
+export const useTaskList = () => {
+  return useQuery({
+    queryKey: ['taskList'],
+    queryFn: async (): Promise<TaskListItem[]> => {
+      // 과제 목록 전체 조회 api 호출
+      const tasks = await getTaskList();
+
+      // 과제 목록 페이지에 필요한 데이터만 매핑
+      return tasks
+        .filter((task) => !task.dDay.includes('+'))
+        .map((task) => ({
+          id: task.taskId,
+          type: task.type === 'TEAM' ? 'team' : 'personal',
+          folderName: task.folderTitle,
+          assignmentName: task.title,
+          dueDate: task.dDay,
+          // ==============================
+          // 폴더 색상 수정해야함 지금은 관련 없는 값임
+          // ==============================
+          folderColor: FOLDER_COLORS[task.folderId - 2],
+        }));
+    },
+  });
+};
