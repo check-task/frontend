ec036b9193f03deff4d2b5e1ccb713c45eceae29
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-02-05 19:40:23 +0900

    refactor: 더미 데이터 타입 수정-추후 삭제 예정

--- src/constants/AssignmentDataCardMock.ts
+++ src/constants/AssignmentDataCardMock.ts
@@ -1,8 +1,8 @@
-import { type DataItem } from '@/features/assignment/personal/components/PersonalEtc';
+import type { ReferenceItem } from '@/features/assignment/personal/components/PersonalRightContainer';
 
 // ======== 자료 모음집 더미 데이터 ========
 // 0은 url 1은 파일을 의미
-export const DUMMY_DATA: DataItem[] = [
+export const DUMMY_DATA: ReferenceItem[] = [
   { id: 1, type: 0, name: '이클래스', path: 'https://class.tukorea.ac.kr/' },
   { id: 2, type: 1, name: '과제 공지 자료', path: '프로그래밍 제1차 과제.pdf' },
   { id: 3, type: 1, name: '선행연구', path: 'FIFO알고리즘의효과에대하여.pdf' },
