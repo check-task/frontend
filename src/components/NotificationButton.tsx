ff8a91740aaed6f04133229ac8c6bfca38234064
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-02-10 12:12:22 +0900

    feat: 안 읽은 알림 여부 훅을 연결하여 hasUnread에 따라 알림 아이콘이 다르게 보이도록

--- src/components/NotificationButton.tsx
+++ src/components/NotificationButton.tsx
@@ -1,7 +1,14 @@
+'use client';
+
 import { css } from 'styled-system/css';
 import { NotificationIcon } from './icons/NotificationIcon';
+import { NotificationIconActive } from './icons/NotificationIconActive';
+import { useUnreadAlarmCount } from '@/hooks/queries/useUnreadAlarmCount';
 
 export const NotificationButton = () => {
+  const { data } = useUnreadAlarmCount();
+  const hasUnread = data?.hasUnread ?? false;
+
   const notificationButtonStyle = css({
     color: 'blue.600',
     cursor: 'pointer',
@@ -9,7 +16,7 @@ export const NotificationButton = () => {
 
   return (
     <button className={notificationButtonStyle}>
-      <NotificationIcon />
+      {hasUnread ? <NotificationIconActive /> : <NotificationIcon />}
     </button>
   );
 };
