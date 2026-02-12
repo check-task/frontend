51866b5531e7ddb1be878f8f9c446541418f906f
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-02-12 20:44:16 +0900

    refactor: 알림 문구 디자인 속성 수정

--- src/features/alarm/components/AlarmListContainer.tsx
+++ src/features/alarm/components/AlarmListContainer.tsx
@@ -133,7 +133,7 @@ export const AlarmListContainer = () => {
 
       {/* 알림 카드 리스트 */}
       {isLoading ? (
-        <div className={css({ textAlign: 'center' })}>불러오는 중...</div>
+        <div className={css({ textAlign: 'center' })}>불러오는 중입니다.</div>
       ) : visibleAlarms.length === 0 ? (
         <div className={css({ textAlign: 'center' })}>알림이 없습니다.</div>
       ) : (
@@ -165,6 +165,7 @@ const containerStyle = cva({
   base: {
     display: 'flex',
     flexDirection: 'column',
+    color: 'gray.400',
     marginX: 'auto',
     marginY: '3.25rem',
     transition: 'width 0.3s ease-in-out',
