3879f99a3f1701d140d2c8e4c9911128a4261858
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2026-02-12 23:02:15 +0900

    refactor: 사이드바에 따라 화면 너비 다르게 잡히도록 임시 수정

--- src/app/(main)/MainContentWrapper.tsx
+++ src/app/(main)/MainContentWrapper.tsx
@@ -7,21 +7,32 @@ interface MainContentWrapperProps {
   children: React.ReactNode;
 }
 
+const SIDEBAR_OPEN_WIDTH = '15rem';
+
 export const MainContentWrapper = ({ children }: MainContentWrapperProps) => {
   const isSidebarCollapsed = useUIStore((state) => state.isSidebarCollapsed);
 
   return (
     <div
       className={css({
-        width: '100%',
+        // width: '100%',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         pt: '5.25rem',
         // 사이드바가 접혀있을 때는 전체 화면 기준으로 중앙 정렬하기 위해 사이드바 너비만큼 왼쪽으로 이동
         // 사이드바가 펼쳐져있을 때는 main 영역 기준으로 중앙 정렬
-        marginLeft: isSidebarCollapsed ? '0' : '7.5rem',
-        transition: 'margin-left 0.3s ease',
+        // marginLeft: isSidebarCollapsed ? '0' : '7.5rem',
+        // transition: 'margin-left 0.3s ease',
+
+        // 사이드바 상태에 따라 너비를 다르게 설정
+        width: isSidebarCollapsed
+          ? '100%'
+          : `calc(100% - ${SIDEBAR_OPEN_WIDTH})`,
+        marginLeft: isSidebarCollapsed ? '0' : SIDEBAR_OPEN_WIDTH,
+
+        transition:
+          'margin-left 0.3s cubic-bezier(0.4, 0, 0.2, 1), width 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
       })}
     >
       {children}
