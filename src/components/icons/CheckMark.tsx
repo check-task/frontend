6b86fe9d506f7c6d614578e4aacfe2b6966a6994
성태경 <taegyeong0320@naver.com>
2026-04-14 17:23:40 +0900

    fix: 과제 등록 체크 색 수정

--- src/components/icons/CheckMark.tsx
+++ src/components/icons/CheckMark.tsx
@@ -40,6 +40,7 @@ export const CheckMark = ({ variant = 'gray', className }: CheckMarkProps) => {
       height='15'
       viewBox='0 0 15 15'
       fill='none'
+      overflow='visible'
     >
       <path
         d='M0.833008 7.50065L4.58408 13.1273C4.91393 13.622 5.64098 13.622 5.97083 13.1273L14.1663 0.833984'
