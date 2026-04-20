b70626b0eac0aed1f58ff91e95cbcfdf95b55f96
성태경 <taegyeong0320@naver.com>
2026-04-20 21:50:53 +0900

    fix: 온보딩 이미지 svg에서 png로 변경

--- src/constants/previewSlides.ts
+++ src/constants/previewSlides.ts
@@ -5,7 +5,7 @@ export const PREVIEW_SLIDES = [
       '홈화면의 달력으로 이달의 전체 일정을 한 눈에 확인해요.',
       '또, 과제 마감의 우선순위를 직관적으로 알려줘요.',
     ],
-    image: '/login-slide-1.svg',
+    image: '/login-slide-1.png',
   },
   {
     title: '한 눈에 확인하는 과제 진척도',
@@ -13,7 +13,7 @@ export const PREVIEW_SLIDES = [
       '과제별로 세부 TASK와 필요한 자료를 등록해요.',
       'TASK 완료에 따라 과제 진척도를 확인할 수 있어요.',
     ],
-    image: '/login-slide-2.svg',
+    image: '/login-slide-2.png',
   },
   {
     title: '세부 과제별로 팀원들과 소통하기',
@@ -21,6 +21,6 @@ export const PREVIEW_SLIDES = [
       '코드 공유로 팀원을 초대하고 팀프로젝트를 진행해요.',
       'TASK별로 담당자 설정과 의사소통이 가능해요.',
     ],
-    image: '/login-slide-3.svg',
+    image: '/login-slide-3.png',
   },
 ];
--- src/features/login/components/LoginCarousel.tsx
+++ src/features/login/components/LoginCarousel.tsx
@@ -11,21 +11,21 @@ import { RightIcon } from '@/components/icons/RightIcon';
 const SLIDES = [
   {
     index: '1/3',
-    image: '/login-slide-1.svg',
+    image: '/login-slide-1.png',
     title: '명도차이로 확인하는\n과제 마감 우선순위',
     description:
       '홈화면의 달력으로 이달의 전체 일정을 한 눈에 확인해요.\n또, 과제 마감의 우선순위를 직관적으로 알려줘요.',
   },
   {
     index: '2/3',
-    image: '/login-slide-2.svg',
+    image: '/login-slide-2.png',
     title: '한 눈에 확인하는\n과제 진척도',
     description:
       '과제별로 세부 TASK와 필요한 자료를 등록해요.\nTASK 완료에 따라 과제 진척도를 확인할 수 있어요.',
   },
   {
     index: '3/3',
-    image: '/login-slide-3.svg',
+    image: '/login-slide-3.png',
     title: '세부 과제별로\n팀원들과 소통하기',
     description:
       '코드 공유로 팀원을 초대하고 팀프로젝트를 진행해요.\nTASK별로 담당자 설정과 의사소통이 가능해요.',
